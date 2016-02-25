b8255ae670eb9097e244b6fded90dc0c229047e1
matthiasziegler76 <ziegler.golf@googlemail.com>
2016-02-25 13:41:15 +0100

    Minor refactoring

--- GolfLessons/MainTableViewController.swift
+++ GolfLessons/MainTableViewController.swift
@@ -84,6 +84,8 @@ class MainTableViewController: UITableViewController {
     }
     
     
+    // MARK: - TableView delegate
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete{
             
@@ -93,16 +95,14 @@ class MainTableViewController: UITableViewController {
         }
     }
     
+    // MARK: Add lesson
+    
     func addLesson(){
         
         guard let context = fetchedResultsController?.managedObjectContext else{return}
         
-        print("Context we have")
-        
-        
-        
         let customer = NSEntityDescription.insertNewObjectForEntityForName("Customer", inManagedObjectContext: context) as! Customer
-        customer.firstName = "Brigi"
+            customer.firstName = "Brigi"
         
         
         do{
@@ -111,78 +111,10 @@ class MainTableViewController: UITableViewController {
             print("error saving")
         }
         
-//        do{
-//            try fetchedResultsController?.performFetch()
-//        }catch{
-//            print("error fetching")
-//        }
-        
-//        let request = NSFetchRequest(entityName: "Customer")
-//        let pred = NSPredicate(value: true)
-//        let nameSort = NSSortDescriptor(key: "firstName", ascending: true)
-//        //let dateSort = NSSortDescriptor(key: "", ascending: false)
-//        request.sortDescriptors = [nameSort]
-//        request.predicate = pred
-//        request.fetchLimit = 20
-//
-//        do{
-//                let res = try context.executeFetchRequest(request)
-//            print((res.first as! Customer).firstName)
-//                }catch{
-//                      print("error fetching")
-//        }
-        
-        
-        
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
 
 extension MainTableViewController : NSFetchedResultsControllerDelegate{
     
@@ -192,11 +124,8 @@ extension MainTableViewController : NSFetchedResultsControllerDelegate{
     
     func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
         
-        //guard let indexPath = indexPath else{return}
-        
         switch type{
         case .Update:
-            //tableView.reloadRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
             self.configureCell(tableView.cellForRowAtIndexPath(indexPath!)!, indexPath: indexPath!)
         case .Insert: if indexPath != newIndexPath {
             self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
@@ -210,8 +139,6 @@ extension MainTableViewController : NSFetchedResultsControllerDelegate{
             self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
             
         }
-        
-        
     }
     
     func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
